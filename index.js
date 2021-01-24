d2392b9d54d52ee42ebcdeddce49150d3657de51
Dewansh Nehra <68858069+DewanshNehra@users.noreply.github.com>
2021-01-25 04:47:21 +0530

    Update index.js

--- index.js
+++ index.js
@@ -54,4 +54,47 @@ bot.on('time', function() {
 bot.on('spawn',function() {
     connected=1;
 });
+bot.on('chat', (username, message) => {
+    if (username === bot.username) return
+    switch (message) {
+      case 'sleep':
+        goToSleep()
+        break
+      case 'wakeup':
+        wakeUp()
+        break
+    }
+  })
+  
+  bot.on('sleep', () => {
+    bot.chat('Good night!')
+  })
+  bot.on('wake', () => {
+    bot.chat('Good morning!')
+  })
+  
+  async function goToSleep () {
+    const bed = bot.findBlock({
+      matching: block => bot.isABed(block)
+    })
+    if (bed) {
+      try {
+        await bot.sleep(bed)
+        bot.chat("I'm sleeping")
+      } catch (err) {
+        bot.chat(`I can't sleep: ${err.message}`)
+      }
+    } else {
+      bot.chat('No nearby bed')
+    }
+  }
+  
+  async function wakeUp () {
+    try {
+      await bot.wake()
+    } catch (err) {
+      bot.chat(`I can't wake up: ${err.message}`)
+    }
+  }
+
 
